275d94b746a628fe2a6ce3770a0e1dfac9f124a9
aasane kariuki <franciskariki053@gmail.com>
2024-04-15 17:27:06 +0300

    JS writing

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const API_KEY="c0f2c10c5e3ab9f222041e6621b7d837";
+const cityInput=document.querySelector(".city-input");
+const searchButton=document.querySelector(".search-btn")
+const locationButton=document.querySelector(".location-btn")
+const weathercardsDiv=document.querySelector(".Current-weather")
+const currentweatherDiv=document.querySelector(".weather-cards")
+
