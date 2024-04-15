940abc41db32687dfc9d02b03f6a54f4b400057b
aasane kariuki <franciskariki053@gmail.com>
2024-04-15 17:36:07 +0300

    JS running

--- js/index.js
+++ js/index.js
@@ -76,3 +76,24 @@ const getcitycoordinates=()=>{
         alert("An error occured while fetching the coordinates")
     })
 }
+const getusercoordinates=()=>{
+    navigator.geolocation.getCurrentPosition(
+        position=>{
+            const {latitude,longitude}=position.coords;
+            const REVERSE_GEOCODING_URL=`http://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`
+           
+            fetch(REVERSE_GEOCODING_URL).then(res=>res.json()).then(data=>{
+                const {name}=data[0]
+                getWeatherDetails(name,latitude,longitude);
+            }).catch(()=>{
+                alert("An error occured while fetching the city!")
+            });
+        },
+        error=>{
+            if (error.code===error.PERMISSION_DENIED){
+                alert("Geolocation request denied.please reset location permission to grant access again.")
+
+            }
+        }
+    )
+}
