29948f48c82da533deba1d1d4d97d1ab6b742608
aasane kariuki <franciskariki053@gmail.com>
2024-04-15 17:31:42 +0300

    JS writing

--- js/index.js
+++ js/index.js
@@ -5,3 +5,26 @@ const locationButton=document.querySelector(".location-btn")
 const weathercardsDiv=document.querySelector(".Current-weather")
 const currentweatherDiv=document.querySelector(".weather-cards")
 
+const createweathercard= (cityName,weatherItem, index)=>{
+    if(index === 0){
+      return ` <div class="details">
+          <h2>${cityName} (${weatherItem.dt_txt.split(" ")[0]})</h2>
+          <h4>Temperature: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
+          <h4>Wind:${weatherItem.wind.speed}M/S</h4>
+          <h4>Humidity:${weatherItem.main.humidity}%</h4>
+          
+     </div>
+     <div class="icon">
+          <img src= "https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@4x.png" alt="weather-icon">
+          <h4>${weatherItem.weather[0].description}</h4>
+     </div>`;
+    }else{ 
+     return `<li class="card">
+                 <h2>(${weatherItem.dt_txt.split(" ")[0]})</h2>
+                 <img src="https://openweathermap.org/img/wn/${weatherItem.weather[0].icon}@2x.png" alt="weather-icon">
+                 <h4>Temp: ${(weatherItem.main.temp - 273.15).toFixed(2)}°C</h4>
+                 <h4>Wind:${weatherItem.wind.speed}M/S</h4>
+                 <h4>Humidity:${weatherItem.main.humidity}%</h4>
+             </li>`;
+    }
+} 
